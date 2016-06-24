ddf6128a87a5042b8e21bcedf4730947362afe1d
Mark J Radacz <radacz.mcp@gmail.com>
2016-06-24 17:29:19 -0400

    Problem with keyboard pushing the ListView's one row off the screen
    when typing response the keyboard pushes the entire view up and can’t
    see the txt I’m responding from the upper half of the ListView

--- DataTemplateSelector/DataTemplateSelector/DataTemplateSelector/ViewModels/MainPageViewModel.cs
+++ DataTemplateSelector/DataTemplateSelector/DataTemplateSelector/ViewModels/MainPageViewModel.cs
@@ -40,20 +40,20 @@ namespace DataTemplateSelector
             Messages = new ObservableCollection<MessageViewModel>
             {
                 new MessageViewModel { Text = "Hi Squirrel! \uD83D\uDE0A", IsIncoming = true, MessagDateTime = DateTime.Now.AddMinutes(-25)},
-                new MessageViewModel { Text = "Hi Baboon, How are you? \uD83D\uDE0A", IsIncoming = false, MessagDateTime = DateTime.Now.AddMinutes(-24)},
-                new MessageViewModel { Text = "We've a party at Mandrill's. Would you like to join? We would love to have you there! \uD83D\uDE01", IsIncoming = true, MessagDateTime = DateTime.Now.AddMinutes(-23)},
-                new MessageViewModel { Text = "You will love it. Don't miss.", IsIncoming = true, MessagDateTime = DateTime.Now.AddMinutes(-23)},
-                new MessageViewModel { Text = "Sounds like a plan. \uD83D\uDE0E", IsIncoming = false, MessagDateTime = DateTime.Now.AddMinutes(-23)},
-
-                new MessageViewModel { Text = "\uD83D\uDE48 \uD83D\uDE49 \uD83D\uDE49", IsIncoming = false, MessagDateTime = DateTime.Now.AddMinutes(-23)},
-
-
+                //new MessageViewModel { Text = "Hi Baboon, How are you? \uD83D\uDE0A", IsIncoming = false, MessagDateTime = DateTime.Now.AddMinutes(-24)},
+                //new MessageViewModel { Text = "We've a party at Mandrill's. Would you like to join? We would love to have you there! \uD83D\uDE01", IsIncoming = true, MessagDateTime = DateTime.Now.AddMinutes(-23)},
+                //new MessageViewModel { Text = "You will love it. Don't miss.", IsIncoming = true, MessagDateTime = DateTime.Now.AddMinutes(-23)},
+                //new MessageViewModel { Text = "Sounds like a plan. \uD83D\uDE0E", IsIncoming = false, MessagDateTime = DateTime.Now.AddMinutes(-23)},
+                //new MessageViewModel { Text = "\uD83D\uDE48 \uD83D\uDE49 \uD83D\uDE49", IsIncoming = false, MessagDateTime = DateTime.Now.AddMinutes(-23)},
             };
             OutGoingText = null;
             SendCommand = new Command(() =>
             {
-              Messages.Add(new MessageViewModel {Text =  OutGoingText, IsIncoming = false, MessagDateTime = DateTime.Now});
-                OutGoingText = null;
+				if (!string.IsNullOrEmpty (OutGoingText))
+				{
+					Messages.Add (new MessageViewModel { Text = OutGoingText, IsIncoming = false, MessagDateTime = DateTime.Now });
+					OutGoingText = null;
+				} 
             });
         }
        // public List<MessageViewModel> Messages { get; set; } = new List<MessageViewModel>();
